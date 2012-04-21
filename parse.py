8b0643683753c005152c017f91ba7bacc023055b
Harry Rickards <hrickards@gmail.com>
2012-04-21 18:32:57 +0100

    Tweaks

--- parse.py
+++ parse.py
@@ -5,7 +5,8 @@ import lxml.html
 import re
 import csv
 
-writer = csv.writer(open("exams.csv", "w"))
+writer = csv.writer(open("exams_raw.csv", "w"))
+writer2 = csv.writer(open("exams.csv", "w"))
 exams_set = list()
 
 for i, infile in enumerate(glob.glob(os.path.join("data/", "*.html"))):
@@ -19,8 +20,9 @@ for i, infile in enumerate(glob.glob(os.path.join("data/", "*.html"))):
 
     exams = html.get_element_by_id("UCResultsTable_resultsTbl").xpath("//tr[td/@class = 'results noprint']")
     for exam in exams:
-	# details = [re.sub("<br>|\n", " ", re.sub("</?td( class=\"b\")?>|</?(div|input).*>|\t", "", lxml.html.tostring(detail))) for detail in exam.xpath("td[not(input)]")]
-        details = ["<a onClick='event_add($(this))'>%s</a>" % re.sub("<br>|\n", " ", re.sub("</?td( class=\"b\")?>|</?(div|input).*>|\t", "", lxml.html.tostring(detail))) for detail in exam.xpath("td[not(input)]")]
+        # details = ["<a onClick='event_add($(this))'>%s</a>" % re.sub("<br>|\n", " ", re.sub("</?td( class=\"b\")?>|</?(div|input).*>|\t", "", lxml.html.tostring(detail))) for detail in exam.xpath("td[not(input)]")]
+        details = [re.sub("<br>|\n", " ", re.sub("</?td( class=\"b\")?>|</?(div|input).*>|\t", "", lxml.html.tostring(detail))) for detail in exam.xpath("td[not(input)]")]
         if details not in exams_set:
             writer.writerow(details)
+            writer2.writerow(["<a onClick='event_add($(this))'>%s</a>" % detail for detail in details])
             exams_set.append(details)
